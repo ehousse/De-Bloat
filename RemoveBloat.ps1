641544cd941fcb8809590515242f8652ab033372
ehousse <128084837+ehousse@users.noreply.github.com>
2023-12-13 16:20:40 -0500

    Update RemoveBloat.ps1
    Changed Check Version URI

--- RemoveBloat.ps1
+++ RemoveBloat.ps1
@@ -119,7 +119,7 @@ if ($liveversion -ne $currentversion) {
 write-host "Script has been updated, please download the latest version from $liveuri" -ForegroundColor Red
 }
 }
-Get-ScriptVersion -liveuri "https://raw.githubusercontent.com/andrew-s-taylor/public/main/De-Bloat/RemoveBloat.ps1"
+Get-ScriptVersion -liveuri "https://raw.githubusercontent.com/ehousse/Intune-De-Bloat/main/RemoveBloat.ps1"
 
 
 
